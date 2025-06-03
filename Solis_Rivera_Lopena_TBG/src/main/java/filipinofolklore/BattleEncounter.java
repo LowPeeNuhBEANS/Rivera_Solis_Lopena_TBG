37848c3626c78db6c339bbd000318dab6b5d0ff0
enfaith <ssnfmss@gmail.com>
2025-06-03 23:45:52 +0800

    added startBattle method (draft)

--- Solis_Rivera_Lopena_TBG/src/main/java/filipinofolklore/BattleEncounter.java
+++ Solis_Rivera_Lopena_TBG/src/main/java/filipinofolklore/BattleEncounter.java
@@ -13,4 +13,16 @@ public class BattleEncounter {
         this.player = player;
         this.monster = monster;
     }
+
+    public void startBattle() { 
+        // player's turn if player's atkSpeed is greater than or equal to monster's atkSpeed
+        boolean playerFirst = player.getAtkSpeed() >= monster.getAtkSpeed(); 
+        while (player.isAlive() && monster.isAlive()){
+            if (playerFirst) {
+                // player's turn 
+            } else {
+               // monster's turn
+            }
+        }
+    }
 }
