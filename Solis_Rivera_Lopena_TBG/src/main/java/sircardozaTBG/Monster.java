9537c677c0588c77a81425c77a392b31823363ac
enfaith <ssnfmss@gmail.com>
2025-06-03 23:47:28 +0800

    added example monster

--- Solis_Rivera_Lopena_TBG/src/main/java/sircardozaTBG/Monster.java
+++ Solis_Rivera_Lopena_TBG/src/main/java/sircardozaTBG/Monster.java
@@ -6,9 +6,21 @@ public class Monster extends Character {
         super(name, health, minATK, maxATK, armor, speed);
         this.name = super.name;
     }
-    //Sample Monster Spell/skill - for each spell create a new spell/skill method
+
+    // MONSTERS HERE
+    Monster baboyRams = new Monster("Baboy Rams", 50, 1, 10, 1, 1);
+
+    /*Sample Monster Spell/skill - for each spell create a new spell/skill method
     public void roar() {
         System.out.println(name + " lets out a terrifying roar!");
     }
+    */
 
+    public boolean isAlive(){return health > 0;}
+    public int getAtkSpeed(){return (int) super.getSpeed();}
+    public int attack(){return getAttack();}
+    public void takeDamage(int Damage){super.takeDamage(Damage);}  
+    public String getName(){return super.getName();}
+
+    
 }
