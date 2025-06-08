4f514bf18028dd838108f1e519e657e1e12d6e64
enfaith <ssnfmss@gmail.com>
2025-06-09 02:58:32 +0800

    update monsterTurn to use correct monster skills per area

--- Solis_Rivera_Lopena_TBG/src/main/java/filipinofolklore/BattleEncounter.java
+++ Solis_Rivera_Lopena_TBG/src/main/java/filipinofolklore/BattleEncounter.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class BattleEncounter {
     private Player player;
     private Monster monster;
+    private Travel travel;
     private Scanner sc = new Scanner(System.in);
 
-    public BattleEncounter(Player player, Monster monster) {
+    public BattleEncounter(Player player, Monster monster, Travel travel) {
         this.player = player;
         this.monster = monster;
+        this.travel = travel;
     }
 
     public void startBattle() {
         // player's turn if player's atkSpeed is greater than or equal to monster's
-        // atkSpeed
         boolean playerFirst = player.getAtkSpeed() >= monster.getAtkSpeed();
         while (player.isAlive() && monster.isAlive()) {
             if (playerFirst) {
@@ -23,17 +24,18 @@ public class BattleEncounter {
                 String input = sc.nextLine();
                 if (input.equalsIgnoreCase("attack")) {
                     // get the weapon
-                    Weapon weapon = Player.getEquippedWeapon(); 
+                    Weapon weapon = Player.getEquippedWeapon();
                     int weaponDamage = 0;
                     if (weapon != null) {
-                        // this will randomize the weapon damage since weapon is initialized with int min and int max
+                        // this will randomize the weapon damage since weapon is initialized with int
+                        // min and int max
                         weaponDamage = weapon.getMin()
                                 + (int) (Math.random() * (weapon.getMax() - weapon.getMin() + 1));
                     }
                     int damage = player.attack() + weaponDamage; // player attack + weapon damage
                     monster.takeDamage(damage);
-                    System.out.println("You attack " + monster.getName() + " for " + damage 
-                    + " damage! (Weapon: " + weapon.getName() + " dealt " + weaponDamage + ")");
+                    System.out.println("You attack " + monster.getName() + " for " + damage
+                            + " damage! (Weapon: " + weapon.getName() + " dealt " + weaponDamage + ")");
                 }
                 if (!monster.isAlive()) {
                     break;
@@ -49,19 +51,20 @@ public class BattleEncounter {
                 // Player's turn
                 System.out.println("\nYour turn! (Type 'attack' to attack)");
                 String input = sc.nextLine();
-                 if (input.equalsIgnoreCase("attack")) {
+                if (input.equalsIgnoreCase("attack")) {
                     // get the weapon
-                    Weapon weapon = Player.getEquippedWeapon(); 
+                    Weapon weapon = Player.getEquippedWeapon();
                     int weaponDamage = 0;
                     if (weapon != null) {
-                        // this will randomize the weapon damage since weapon is initialized with int min and int max
+                        // this will randomize the weapon damage since weapon is initialized with int
+                        // min and int max
                         weaponDamage = weapon.getMin()
                                 + (int) (Math.random() * (weapon.getMax() - weapon.getMin() + 1));
                     }
                     int damage = player.attack() + weaponDamage; // player attack + weapon damage
                     monster.takeDamage(damage);
-                    System.out.println("You attack " + monster.getName() + " for " + damage 
-                    + " damage! (Weapon: " + weapon.getName() + " dealt " + weaponDamage + ")");
+                    System.out.println("You attack " + monster.getName() + " for " + damage
+                            + " damage! (Weapon: " + weapon.getName() + " dealt " + weaponDamage + ")");
                 }
             }
         }
@@ -73,13 +76,61 @@ public class BattleEncounter {
         }
     }
 
-    
-    // this is only temporary, special ability not yet coded
-    // basic attack of the monster 
     public void monsterTurn() {
-        int damage = monster.attack();
-            player.takeDamage(damage);
-            System.out.println(monster.getName() + " attacks you for " + damage + " damage!");
-    }
+        // I made it this way instead of having many else if statements.
+        // This gets the AreaCounter and will match the current monster for the turn.
+        // Normal attack method is in MonsterSkill.java
+        // Woods
+        if (travel.getAreaCounter() == 1) {
+            if (monster.getName().equals("Sigbin")) {
+                MonsterSkill.sigbinSkill(monster, player);
+            }
+            if (monster.getName().equals("Kapre")) {
+                MonsterSkill.sigbinSkill(monster, player);
+            }
+        }
 
+        // Swamp
+        if (travel.getAreaCounter() == 2) {
+            if (monster.getName().equals("Berberoka")) {
+                MonsterSkill.sigbinSkill(monster, player);
+            }
+            if (monster.getName().equals("Siyokoy")) {
+                MonsterSkill.sigbinSkill(monster, player);
+            }
+            if (monster.getName().equals("Mutated Carabao")) {
+                MonsterSkill.sigbinSkill(monster, player);
+            }
+        }
+
+        // Village
+        if (travel.getAreaCounter() == 3) {
+            if (monster.getName().equals("Manananggal")) {
+                MonsterSkill.sigbinSkill(monster, player);
+            }
+            if (monster.getName().equals("Sarangay")) {
+                MonsterSkill.sigbinSkill(monster, player);
+            }
+            if (monster.getName().equals("Tiyanak")) {
+                MonsterSkill.sigbinSkill(monster, player);
+            }
+            if (monster.getName().equals("Diwata")) {
+                MonsterSkill.sigbinSkill(monster, player);
+            }
+        }
+
+        // Mountain
+        if (travel.getAreaCounter() == 4) {
+            if (monster.getName().equals("Sigbin")) {
+                MonsterSkill.sigbinSkill(monster, player);
+            }
+        }
+
+        // Ocean
+        if (travel.getAreaCounter() == 5) {
+            if (monster.getName().equals("Sigbin")) {
+                MonsterSkill.sigbinSkill(monster, player);
+            }
+        }
+    }
 }
