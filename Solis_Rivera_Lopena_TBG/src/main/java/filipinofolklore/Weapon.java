f7415225899dbd31961697ed038b4754e140c41d
Vincent Lopena <57670705+LowPeeNuhBEANS@users.noreply.github.com>
2025-06-09 20:24:30 +0800

    Add files via upload
    I want to jump off the highest building in Davao City, anyway here are the changes:
    -Added HealthBar.Java class [Stack based health bar UI]
    -Added biome based monster drop [INCOMPLETE]
    -Colors
    
    NEEDS TO BE ADDED/FIXED:
    -Player and Monster Health bar display
    -Player Username display.
    -Randomization of item dropped by monsters

--- Solis_Rivera_Lopena_TBG/src/main/java/filipinofolklore/Weapon.java
+++ Solis_Rivera_Lopena_TBG/src/main/java/filipinofolklore/Weapon.java
@@ -1,14 +1,14 @@
 package filipinofolklore;
 
-/*This file contains the Weapon class. All stats and getters of a Weapon object can be found here*/
+import java.lang.Math;
+
 public class Weapon {
 
-    int minAtk; //min dmg
-    int maxAtk; //max dmg
-    int atkSpeed; //speed increase/decrease
-    String name; //Weapon name
+    int minAtk;
+    int maxAtk;
+    int atkSpeed;
+    String name;
 
-    //Weapon Constructor
     public Weapon(String name, int min, int max, int speed) {
         this.name = name;
         minAtk = min;
@@ -16,27 +16,26 @@ public class Weapon {
         atkSpeed = speed;
     }
 
-    //Get weapon name
     public String getName() {
         return name;
     }
-    //Get weapon min dmg
+
     public int getMin() {
         return minAtk;
     }
-    //Get weapon max dmg
+
     public int getMax() {
         return maxAtk;
     }
-    //Get weapon speed points
+
     public int getSpeed() {
         return atkSpeed;
     }
-    //Get message of weapon speed
+
     public String getSpeedMsg() {
         if (atkSpeed > 0) {
             return "It increases your speed by " + atkSpeed + " points.";
-                    }
+        }
         return "It decreases your speed by " + Math.abs(atkSpeed) + " points.";
     }
 }
